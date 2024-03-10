fe8cf7fc56fcafc6bc998abdcdf4ef6ef531b725
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-03-11 00:40:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Route, Router, Routes } from 'react-router-dom';
-import Calculate from "./Calculate"
+import { Route, Routes } from 'react-router-dom';
+import Counter from './Calculate';
+
 function App() {
   // Initialize the counter state to 0
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  // Function to increment the counter
-  const increment = () => setCount(count + 1);
+  // // Function to increment the counter
+  // const increment = () => setCount(count + 1);
 
-  // Function to decrement the counter
-  const decrement = () => setCount(count - 1);
+  // // Function to decrement the counter
+  // const decrement = () => setCount(count - 1);
 
-  // Function to reset the counter
-  const reset = () => setCount(0);
+  // // Function to reset the counter
+  // const reset = () => setCount(0);
 
   return (
     <div className="App" style={{ width: '500px', height: '400px' }}>
@@ -22,14 +23,19 @@ function App() {
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button> */}
+      <ul>
+        <l1>
+          <a href="#/">Home</a>
+          <a href="#/about">Home</a>
+        </l1>
+      </ul>
 
-<Router>
-      <Routes>
+      {/* <Routes>
         
-        <Route path="/" element={<Calculate/>} />
+        <Route path="/" element={<Counter/>} />
         
-      </Routes>
-    </Router>
+      </Routes> */}
+    
     </div>
   );
 }
